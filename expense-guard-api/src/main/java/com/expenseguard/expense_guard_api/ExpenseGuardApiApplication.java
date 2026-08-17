afedfec53909c1eab9a7c22eeffbbaa6fabc330d
kinchung99 <kinchung99@github.com>
2026-08-17 22:26:09 +0800

    Initialized Maven Project

---
+++ expense-guard-api/src/main/java/com/expenseguard/expense_guard_api/ExpenseGuardApiApplication.java
@@ -0,0 +1,13 @@
+package com.expenseguard.expense_guard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseGuardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseGuardApiApplication.class, args);
+	}
+
+}
---
+++ expense-guard-api/src/test/java/com/expenseguard/expense_guard_api/ExpenseGuardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseguard.expense_guard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseGuardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
